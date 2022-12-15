341a6e0a718acbd5371ef53d291c23cf655c7a94
aodtmdals <aodtmdals12@naver.com>
2022-12-15 16:28:47 +0900

    디테일 수정

--- miniproject/src/main/resources/static/js/header.js
+++ miniproject/src/main/resources/static/js/header.js
@@ -1,5 +1,16 @@
     $(document).ready(function() {
- 
+	 	var header = $('header');
+	
+	    $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+	            if(!header.hasClass('shadow')){
+	                header.addClass('shadow');
+	            }
+	        }else{
+	            header.removeClass('shadow');
+	        }
+	    });
+	    
         $("#upBtnBox").hide();
         $(function() {
  
